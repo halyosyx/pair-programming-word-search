9efd67b4cbae21bd0ca1a6fb2ce3de79b623dc4c
vagrant <aisle.github@gmail.com>
2021-11-03 23:46:52 +0000

    Fixed bugs and impemented the ability to search vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  // checking horizontals
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (const line of horizontalJoin) {
+    if (line.includes(word)) return true
+  }
+
+  const endArray = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    endArray.push([]);
+    for (let j = 0; j < letters.length; j++) {
+      endArray[i].push(letters[j][i]);
     }
+  }
+  
+  // checking vertical
+  const verticalJoin = endArray.map(ls => ls.join(''))
+  for (const line of verticalJoin) {
+    if (line.includes(word)) return true
+  }
+
+  return false
 }
 
 module.exports = wordSearch
